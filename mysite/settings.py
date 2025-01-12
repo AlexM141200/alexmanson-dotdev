023452ada6f8ded2aa08c14e1d8dd873481fa676
AlexM141200 <alex.manson14@gmail.com>
2025-01-12 20:57:22 +0000

    Added first migrations

--- mysite/settings.py
+++ mysite/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "alexmansondotdev.apps.AlexmansondotdevConfig",
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
